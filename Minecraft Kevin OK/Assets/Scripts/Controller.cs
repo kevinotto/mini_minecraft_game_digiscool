9dbcde4aa9a1bc3b29a0d0e4e9ef4d667d1936bc
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2023-01-28 20:07:29 +0200

    v2

--- Minecraft Kevin OK/Assets/Scripts/Controller.cs
+++ Minecraft Kevin OK/Assets/Scripts/Controller.cs
@@ -24,5 +24,48 @@ public class Controller : MonoBehaviour
     {
         Pitch += Input.GetAxis("Mouse Y") * CameraSpeed.x * Time.deltaTime;
         Yaw += Input.GetAxis("Mouse X") * CameraSpeed.y * Time.deltaTime;
+
+        Camera.main.transform.eulerAngles = new Vector3(Pitch, Yaw, 0);
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Fly");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 dirForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
+        Vector3 dirSide = Camera.main.transform.right;
+        Vector3 dirUp = Vector3.up;
+        Vector3 moveDir = (inputX * dirSide) + (inputY * dirUp) + (inputZ * dirForward);
+
+        Camera.main.transform.position += moveDir * MoveSpeed * Time.deltaTime;
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if(Physics.Raycast(ray, out Hit))
+        {
+            Vector3 pos = Hit.point;
+            pos += Hit.normal * 0.1f;
+            pos = new Vector3(Mathf.Floor(pos.x), Mathf.Floor(pos.y), Mathf.Floor(pos.z));
+            pos += Vector3.one * 0.7f;
+            Outline.transform.position = pos;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(BlockPrefab, pos, Quaternion.identity);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                if (Hit.collider.name == "Block(Clone)")
+                {
+                    Destroy(Hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 9999);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(Hit.point, 0.05f);
+        Gizmos.DrawRay(Hit.point, Hit.normal);
     }
 }
